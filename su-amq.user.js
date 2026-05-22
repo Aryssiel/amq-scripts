53619cded9a47fbf1c2f735026c038b7c3976e19
Zheva <116414188+Aryssiel@users.noreply.github.com>
2026-05-22 19:22:24 -0400

    Initial GitHub release

---
+++ su-amq.user.js
@@ -0,0 +1,317 @@
+// ==UserScript==
+// @name         su amq
+// @namespace    lol
+// @version      0.6
+// @description  Button to create a lobby right away with preselected names and password..
+// @author       ary ft. chatgpt
+// @match        https://*.animemusicquiz.com/*
+// @grant        none
+// ==/UserScript==
+'use strict';
+let names = ["sharakuse","yippe","nicolasbruna","jafeto","jaimeme","metak weko"]
+let hostListner = null;
+let isTeams = false;
+let communityMode = false;
+let roomName = "test"
+let password = "123";
+this.hostListner;
+
+
+function onViewChanged() {
+    ViewChanger.prototype.changeView = (function() {
+        let old = ViewChanger.prototype.changeView;
+        return function() {
+            old.apply(this, arguments);
+            setTimeout(() => { onViewChanged(); }, 1);
+        }
+    })();
+    function onViewChanged() {
+        if(viewChanger.currentView === "lobby") {
+            readyStart();
+        }
+    }
+}
+// Function to be executed when the button is clicked
+let settings = {
+    "roomName": roomName,
+    "privateRoom": true,
+    "password": password,
+    "roomSize": 8,
+    "numberOfSongs": 100,
+    "teamSize": 8,
+    "modifiers": {
+        "skipGuessing": true,
+        "skipReplay": true,
+        "duplicates": true,
+        "queueing": true,
+        "lootDropping": true,
+        "rebroadcastSongs": true,
+        "dubSongs": false,
+        "fullSongRange": true
+    },
+    "songPool": 2,
+    "songSelection": {
+        "standardValue": 1,
+        "advancedValue": {
+            "watched": 0,
+            "unwatched": 0,
+            "random": 100
+        }
+    },
+    "watchedDistribution": 1,
+    "songType": {
+        "standardValue": {
+            "openings": true,
+            "endings": true,
+            "inserts": true
+        },
+        "advancedValue": {
+            "openings": 0,
+            "endings": 0,
+            "inserts": 0,
+            "random": 100
+        }
+    },
+    "openingCategories": {
+        "instrumental": true,
+        "chanting": true,
+        "character": true,
+        "standard": true
+    },
+    "endingCategories": {
+        "instrumental": true,
+        "chanting": true,
+        "character": true,
+        "standard": true
+    },
+    "insertCategories": {
+        "instrumental": true,
+        "chanting": true,
+        "character": true,
+        "standard": true
+    },
+    "guessTime": {
+        "randomOn": false,
+        "standardValue": 20,
+        "randomValue": [
+            1,
+            60
+        ]
+    },
+    "extraGuessTime": {
+        "randomOn": false,
+        "standardValue": 0,
+        "randomValue": [
+            0,
+            15
+        ]
+    },
+    "scoreType": 1,
+    "hintSetup": {
+        "songPoints": 5,
+        "nameCost": 4,
+        "infoCost": 1,
+        "multipleChoiceCost": 4,
+        "audioCost": 3,
+        "tinyVideoCost": 3,
+        "blurVideoCost": 3
+    },
+    "answeringMode": 1,
+    "guessMode": {
+        "song": true,
+        "tinyVideo": false,
+        "blurVideo": false
+    },
+    "showSelection": 1,
+    "inventorySize": {
+        "randomOn": false,
+        "standardValue": 20,
+        "randomValue": [
+            1,
+            99
+        ]
+    },
+    "lootingTime": {
+        "randomOn": false,
+        "standardValue": 90,
+        "randomValue": [
+            10,
+            150
+        ]
+    },
+    "lives": 5,
+    "bossLives": 3,
+    "bossPowerUps": 3,
+    "bossMaxSongs": 10,
+    "samplePoint": {
+        "randomOn": true,
+        "standardValue": 1,
+        "randomValue": [
+            0,
+            100
+        ]
+    },
+    "playbackSpeed": {
+        "randomOn": false,
+        "standardValue": 1,
+        "randomValue": [
+            true,
+            true,
+            true,
+            true
+        ]
+    },
+    "songDifficulity": {
+        "advancedOn": true,
+        "standardValue": {
+            "beginner": true,
+            "easy": true,
+            "medium": true,
+            "hard": true,
+            "expert": true
+        },
+        "advancedValue": [
+            0,
+            50
+        ]
+    },
+    "songPopularity": {
+        "advancedOn": false,
+        "standardValue": {
+            "disliked": true,
+            "mixed": true,
+            "liked": true
+        },
+        "advancedValue": [
+            0,
+            100
+        ]
+    },
+    "playerScore": {
+        "advancedOn": false,
+        "standardValue": [
+            1,
+            10
+        ],
+        "advancedValue": [
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+        ]
+    },
+    "animeScore": {
+        "advancedOn": false,
+        "standardValue": [
+            2,
+            10
+        ],
+        "advancedValue": [
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+        ]
+    },
+    "animePopularity": {
+        "advancedOn": false,
+        "standardValue": 5,
+        "advancedValue": [
+            1,
+            9999
+        ]
+    },
+    "vintage": {
+        "standardValue": {
+            "years": [
+                1924,
+                2026
+            ],
+            "seasons": [
+                0,
+                3
+            ]
+        },
+        "advancedValueList": []
+    },
+    "type": {
+        "tv": true,
+        "movie": true,
+        "ova": true,
+        "ona": true,
+        "special": true
+    },
+    "genre": [],
+    "tags": [],
+    "gameMode": "Multiplayer"
+};
+function handleClick() {
+    if (this.hostListner) {
+		this.hostListner.unbindListener();
+	}
+	this.hostListner = new Listener(
+		"Host Game",
+		function (response) {
+			hostModal.hide();
+			lobby.setupLobby(response, false);
+			viewChanger.changeView("lobby");
+			if (this.hostListner) {
+				this.hostListner.unbindListener();
+			}
+		}.bind(this)
+	);
+
+	this.hostListner.bindListener();
+    settings.roomName = names[Math.floor(Math.random() * names.length)];
+
+
+    socket.sendCommand({
+		type: "roombrowser",
+		command: "host room",
+		data: {settings, communityMode}
+	});
+}
+
+// Create the button element
+var button = document.createElement("div");
+button.className = "button floatingContainer mainMenuButton";
+button.id = "customButton";
+button.innerHTML = "<h1>Su amq</h1>";
+//button.style.padding = "10px";
+//button.style.marginLeft = "15%";
+//button.style.display = "flex";
+//button.style.color = "white";
+//button.style.cursor = "pointer";
+
+// Add a click event listener to the button
+button.addEventListener("click", handleClick);
+
+// Append the button to the main menu
+var mainMenu = document.getElementById("mainMenu");
+if (mainMenu) {
+    mainMenu.insertBefore(button, mainMenu.firstChild);
+}
+
+// Add the button to the options container as well, if needed
+var optionsContainer = document.getElementById("optionsContainer");
+if (optionsContainer) {
+    var optionsList = optionsContainer.querySelector("ul");
+    if (optionsList) {
+        var listItem = document.createElement("li");
+        listItem.className = "clickAble";
+        listItem.textContent = "Custom Button";
+        listItem.addEventListener("click", handleClick);
+        optionsList.insertBefore(listItem, optionsList.firstChild);
+    }
+}
